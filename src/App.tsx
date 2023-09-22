bcdde5e19f5e89fa0d2da083298caf5c278576cf
Devex <devex@Devexs-MacBook-Air.local>
2023-09-22 12:43:06 +0200

    Move Banner out of layout folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/layout/NavBar/NavBar';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <NavBar />
       </header>
     </div>
