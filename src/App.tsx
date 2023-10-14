07dc0a6bc55525710499cb0818657127dbdb0f9a
xavi666 <xavier.garces@devex.com>
2023-10-14 16:46:30 +0200

    Remove context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import { createContext } from "react";
-
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
-
-export const ThemeContext = createContext(null);
 
 const App = () => {
   return (
