d3b2421a7b1a54def8672dbcdad9dac9a7b689db
xavi666 <xavier.garces@devex.com>
2023-10-14 17:28:45 +0200

    Small css adjusments + Festes skeleton

--- src/components/pages/Festes.tsx
+++ src/components/pages/Festes.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+// Bootstrap elements
+import { Container, Row, Col } from "react-bootstrap";
+
 const Festes = () => {
-  return <div className="festes">Festes</div>;
+  return (
+    <section className="bg-dark text-light pt-3 pb-3" id="festes">
+      <Container>
+        <Row>
+          <Col>
+            <h1>Festes</h1>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Festes;
